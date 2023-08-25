8ca3514a464ad689dde16b3e6424828b7090f628
Trevor Hartman <100978684+thartmanoftheredwoods@users.noreply.github.com>
2023-08-25 14:57:03 -0700

    Update README.md
    Fixed Images

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
-
+/*   Chris Shortt's homework Java Assignment-002 */
 public class ArchimedesPiMethod {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        System.out.println("please type the number of sides");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double b =  360.0 / n;
+        double a = b / 2;
+        double s = 2 * Math.sin(Math.toRadians(a));
+        double p = n * s;
+        double pi = p / 2;
+        System.out.printf("Our PI estimate is: %f", pi );
     }
 }
