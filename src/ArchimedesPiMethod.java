2c42f1c05fbb622f077e25a3885e8475027310d6
Atticus Blanco <atticusbrujx@gmail.com>
2023-08-30 14:53:08 -0700

    bing bong

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many sides does your polygon have? ");
+        int nPolygonSides = Integer.parseInt(scanner.nextLine().trim());
+        double topAngle = 360/nPolygonSides;
+        double congruentAngle = (360 - topAngle)/2;
+        System.out.println("Your polygon of " + nPolygonSides + " can be divided into " + nPolygonSides + " triangles,");
+        System.out.println("whose angles are " + topAngle + ", " + congruentAngle + ", and " + congruentAngle);
 
     }
 }
