04db71e1002966263db7df61b3d2b18eb93ff4a5
Atticus Blanco <atticusbrujx@gmail.com>
2023-08-30 23:01:04 -0700

    bing bong

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,32 +2,35 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-        //Step 1: Read into an integer the number of sides for our polygon: $n = 8$
+        //Step 1: Read into an integer the number of sides for our polygon
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many sides does your polygon have? ");
-        int nPolygonSides = Integer.parseInt(scanner.nextLine().trim());
+        int PolygonSidesInt = Integer.parseInt(scanner.nextLine().trim());
+        double PolygonSidesDouble = Double.valueOf(PolygonSidesInt);
 
-        //Step 2: Calculate the angle $B = {360.0 \over n}$
-        double b = 360/nPolygonSides;
-        System.out.println(b);  //test point
+        //Step 2: Calculate the angle b
+        double b = 360/PolygonSidesDouble;
+        System.out.println("b = " + b);  //test point
 
-        //Step 3: Calculate the angle $A = {1 \over 2} * B$
+        //Step 3: Calculate the angle a
         double a = b/2;
-        System.out.println(a);      //test point
-        //Step 4: Calculate the length of 1 triangle base $s = 2 * sin(A)$ HINT: YOU NEED TO CONVERT A to RADIANS
+        System.out.println("a = " + a);      //test point
+
+        //Step 4: Calculate the length of 1 triangle base
         a = Math.toRadians(a);
-        System.out.println(a);      //test point
-
+        System.out.println("a = " + a);      //test point
         double s = 2 * Math.sin(a);
-        System.out.println(s);      //test point
+        System.out.println("s = " + s);      //test point
 
-        //Math.sin(), and Math.toRadians() will be your friends.
+        //Step 5: Get the polygon perimeter
+        double polygonPerimeter = s * PolygonSidesDouble;
+        System.out.println("The perimeter of a " + PolygonSidesInt + "-sided polygon inscribed in a unit circle is "
+                + polygonPerimeter + " units.");
 
-
-        //Step 5: Get the polygon perimeter $p = n * s$
-
-
-        //Step 6: Estimate PI: $PI = {p \over 2}$
-
+        //Step 6: Estimate PI
+        double polygonSemiPerimeter = polygonPerimeter/2;
+        System.out.println("The  semiperimeter is " + polygonSemiPerimeter);
+        double percentageAccuracy = (polygonSemiPerimeter/Math.PI)*100;
+        System.out.println("This Archimedian estimation of π is " + percentageAccuracy + "% accurate.");
     }
 }
