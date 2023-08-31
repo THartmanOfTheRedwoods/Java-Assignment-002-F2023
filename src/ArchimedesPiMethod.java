67245d965130aaa13bc391a56e9e96401e07903b
Atticus Blanco <atticusbrujx@gmail.com>
2023-08-31 12:10:19 -0700

    bing bong

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -6,24 +6,25 @@ public class ArchimedesPiMethod {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many sides does your polygon have? ");
         int PolygonSidesInt = Integer.parseInt(scanner.nextLine().trim());
-        double PolygonSidesDouble = Double.valueOf(PolygonSidesInt);
+        //double PolygonSidesDouble = (double) PolygonSidesInt;
 
         //Step 2: Calculate the angle b
-        double b = 360/PolygonSidesDouble;
-        System.out.println("b = " + b);  //test point
+        double b = 360/((double) PolygonSidesInt);
+        System.out.println("Your " + PolygonSidesInt + "-sided polygon is comprised of " + PolygonSidesInt + " equal isosceles triangles\n" +
+                        "that each have one angle of " + b + "° and two congruent angles of " + (180-b)/2 + "°.");  //test point
 
         //Step 3: Calculate the angle a
         double a = b/2;
-        System.out.println("a = " + a);      //test point
+        System.out.println("\nEach isosceles triangle is comprised of two right triangles with angles " + a + "°, " + (90-a) + "°, and 90°." );      //test point
 
         //Step 4: Calculate the length of 1 triangle base
         a = Math.toRadians(a);
         System.out.println("a = " + a);      //test point
         double s = 2 * Math.sin(a);
-        System.out.println("s = " + s);      //test point
+        System.out.println("The sin of " + a + "° is " + s);      //test point
 
         //Step 5: Get the polygon perimeter
-        double polygonPerimeter = s * PolygonSidesDouble;
+        double polygonPerimeter = s * ((double) PolygonSidesInt);
         System.out.println("The perimeter of a " + PolygonSidesInt + "-sided polygon inscribed in a unit circle is "
                 + polygonPerimeter + " units.");
 
