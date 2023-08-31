5653a3f2749798b8381998e7afbd8e49fd4c5ac9
Jesus Ruiz <jesus.ruiz4792@gmail.com>
2023-08-30 19:43:31 -0700

    Assignment 2 Done

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,30 @@
-import java.util.Scanner;
-
 public class ArchimedesPiMethod {
+
     public static void main(String[] args) {
 
+        int n = 8;
+
+        int b = 360/n;
+
+        System.out.println(b);
+
+        double a = 0.5 * b;
+
+        System.out.println(a);
+
+        double convert = Math.toRadians(a);
+
+        System.out.println(convert);
+
+        double s = 2 * Math.sin(convert);
+
+        System.out.println(s);
+
+        double p = n * s;
+
+        System.out.println(p);
+
+        double pi = p/2;
+        System.out.println("PI Estimate: "+ pi );
     }
 }
