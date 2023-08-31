385e642dbe01be73c370fee43a4299516f5e1af8
Atticus Blanco <atticusbrujx@gmail.com>
2023-08-30 21:24:04 -0700

    bing bong

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -8,14 +8,18 @@ public class ArchimedesPiMethod {
         int nPolygonSides = Integer.parseInt(scanner.nextLine().trim());
 
         //Step 2: Calculate the angle $B = {360.0 \over n}$
-        int b = 360/nPolygonSides;
-        int a = b/2;
+        double b = 360/nPolygonSides;
+        System.out.println(b);  //test point
 
         //Step 3: Calculate the angle $A = {1 \over 2} * B$
-
-
+        double a = b/2;
+        System.out.println(a);      //test point
         //Step 4: Calculate the length of 1 triangle base $s = 2 * sin(A)$ HINT: YOU NEED TO CONVERT A to RADIANS
+        a = Math.toRadians(a);
+        System.out.println(a);      //test point
 
+        double s = 2 * Math.sin(a);
+        System.out.println(s);      //test point
 
         //Math.sin(), and Math.toRadians() will be your friends.
 
