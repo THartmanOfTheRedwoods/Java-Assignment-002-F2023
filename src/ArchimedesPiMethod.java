676f85a33495efae7d2ece480376896f89c44d42
lance <fragasteven148@gmail.com>
2023-08-31 14:11:48 -0700

    Mission complete...

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        int n = 8;
+        double totalDegree = 360.0;
+        double angleB = (totalDegree / n);
+        double angleA = (double) 1 / 2 * angleB;
+        double s = Math.toRadians(angleA) * 2;
+        double p = n * s;
+        double PI =p / 2;
+        System.out.println("This is an estimate of PI: "+PI);
     }
 }
+
