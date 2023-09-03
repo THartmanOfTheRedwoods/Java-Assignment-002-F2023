e53fb193cc8cca328e1a87cca36171d6c768c2e8
Ethan <ethanj955@gmail.com>
2023-09-02 20:03:08 -0700

    Assignment 2

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+        System.out.println("Please type the number of sides. ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        double b = 360.0 / n;
+        double a = b / 2;
+        double s = 2 * Math.sin(Math.toRadians(a));
+        double p = n * s;
+        double pi = p / 2;
+        System.out.println("Out pi estimate is:" + pi);
     }
 }
