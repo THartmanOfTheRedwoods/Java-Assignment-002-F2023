ff4f613136d4aa3a25d0c8403e6893ac248ef1e5
Rayman4009 <tuckeravenmarg@gmail.com>
2023-09-03 11:16:16 -0700

    Why is it 0

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,13 @@
-import java.util.Scanner;
-
+import java.lang.Math;
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        int n = 8;
+        int b = 360 / n;
+        int a = (1/2) * b;
+        double newA = Math.toRadians(a);
+        double s = 2 * Math.sin(newA);
+        double p = n * s;
+        double PI = p / 2;
+        System.out.println(PI);
     }
 }
