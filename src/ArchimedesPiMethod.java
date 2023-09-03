b75ff7d3abb726ae21f1a128c3a49861a6a65dfb
Daniel Faulkner <dfaulkner707@gmail.com>
2023-09-02 21:43:35 -0700

    finished assignment 002

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+int n = 8;
+double b = 360.0/n;
+double a = Math.toRadians(1.0 / 2.0 * b);
+double s = 2.0 * Math.sin(a);
+double p = Math.PI * s;
+double pi = p / 2.0;
+System.out.println("PI =" + pi);
     }
 }
