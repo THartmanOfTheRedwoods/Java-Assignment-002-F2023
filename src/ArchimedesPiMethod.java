7b640d0e4dae0841533d4b72d9e96bc532bf2f06
Lina <lina.guts12@gmail.com>
2023-09-05 20:23:06 -0700

    Completed Java Assignment 002

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,25 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Angelina Perez
+ *
+ * @since Version 1.0
+ *
+ */
+
 import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+        System.out.println("Type the number of polygon sides.");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double b = 360.0 / n;
+        double a = b / 2;
+        double s = 2 * Math.sin(Math.toRadians(a));
+        double p = n * s;
+        double pi = p / 2;
 
+        System.out.println(pi);
     }
 }
