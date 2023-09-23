05d2d0de26ff11d0a6b5d1dd9ac5ee29f060c99f
LiviMcK <137836813+LiviMcK@users.noreply.github.com>
2023-09-23 16:11:32 -0700

    I tried my best to finish this lab, I couldn't finish the code and make it work past this point.

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+    int n = 8;
+    double angleB = 360.0 / n;
+        System.out.println("Angle B for a " + n + "-sided polygon is: " + angleB + " degrees");
+    double angleA = 0.5 * angleB;
+        System.out.println("Angle A is: " + angleA + " degrees");
+        double s = 2.0 * Math.sin(Math.toRadians(angleA));
+        System.out.println("Length of the triangle base (s) is: " + s);
+        class PolygonPerimeterCalculator {
+            public static void main(String[] args) {
+                int n = 6;
+                double s = 0.7653668647301796;
+                double perimeter = n * s;
+
+                System.out.println(n * s);
+            }
+        }
 
     }
 }
