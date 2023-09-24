a86e63cc3de928b8d481cfcf87e8c9e3df533b2d
sjmeans <skylerjmeans@gmail.com>
2023-09-23 18:34:16 -0700

    assignment completed

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        while(true){
+            System.out.print("Please input the amount of sides in your desired polygon: ");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if(n < 1) {
+                System.out.println("Cheeky, try again.");
+                break;
+            }
+            System.out.printf("You chose a polygon of %d sides.\n", n);
+            double b = 360.0 / n;
+            double a = b / 2;
+            double s = 2 * Math.sin(Math.toRadians(a));
+            double p = n * s;
+            System.out.println("You've calculated an estimation of pi resulting to: " + p / 2.0 + ", close, but no cigar!");
+        }
     }
 }
