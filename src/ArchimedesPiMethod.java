ba6d4112a43bd2a864054d45b43ebe90d876c700
MJFracess <mjfracess@gmail.com>
2023-09-24 13:38:29 -0700

    Not throwing darts but I do feel lost!

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,30 @@
 import java.util.Scanner;
 
+/**
+ * @author Trevor Hartman
+ * @author MJ Fracess
+ *
+ * @since Version 1.0
+ */
 public class ArchimedesPiMethod {
-    public static void main(String[] args) {
+    public static void main(String[] arg){
+        while(true){
+            System.out.println("Please Enter the number of sides.");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if(n < 1) {
+                break;
+            }
+            double b = 360.0 / n;
+            double a = b / 2;
+            double s = 2 * Math.sin(Math.toRadians(a));
+            double p = n * s;
+            double pi = p / 2;
+            System.out.printf("Our PI estimate is: %.10f%n", pi);
+            System.out.println(pi);
+    }
+
+
 
     }
 }
