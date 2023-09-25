ef19e83cbc9b444330b4992d6da6b9d8f1c9ff23
fires <mlarose859@mycr.redwoods.edu>
2023-09-24 18:39:10 -0700

    Completed Assignment 002

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        int n = 376;
+        int B = 360;
+        double A = .5;
+        int s = 2;
+       System.out.println(B/n);
+       System.out.println(A*B);
+       System.out.println(n*(Math.toRadians(s*Math.sin(A)))/2);
     }
 }
