399f598b0b3526b6084e8ff5b03b8a9fbc103cb0
dyoungxboxpro <dyoung367@mycr.redwoods.edu>
2023-09-24 18:42:18 -0700

    Completed Assignment 2

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,23 @@
+
 import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+        while(true) {
+            System.out.println("please type the number of sides.");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if(n < 1) {
+                break;
+            }
+            double b = 360.0/n;
+            double a = b / 2;
+            double s = 2 * Math.sin(Math.toRadians(a));
+            double p = n * s;
+            double pi = p / 2;
+            System.out.printf("our PI estimate is: %.10f%n", pi );
+        }
+
 
     }
 }
