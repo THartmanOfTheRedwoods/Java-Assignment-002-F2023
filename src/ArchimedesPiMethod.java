d4b5466045839aaeba6f20ac6e64d15e2362be53
anakin <anakinprice69@gmail.com>
2023-09-26 20:12:54 -0700

    made circle

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,21 @@
 import java.util.Scanner;
-
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
+        while(true) {
+            System.out.println("pls type number of sides");
+            Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+                if(n<1) {
+                    break;
+                }
+            double b = 360.0/n;
+            double a = b/2;
+            double s = 2*Math.sin(Math.toRadians(a));
+            double p = n*s;
+            double pi = p/2;
+            System.out.printf("our pi guess is %f%n", pi);
+
+        }
 
     }
 }
