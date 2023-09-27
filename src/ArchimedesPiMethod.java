3606039a470265570ad823c71ea218b216d4f567
2sage4thyme <2sage4thyme@gmail.com>
2023-09-27 10:29:13 -0700

    Done..? *finger guns*

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-    double n = 8.0;
-                 double b = 360.0/n;
-               double A = (1.0/2.0)* b;
-               double s = 2.0* Math.sin(Math.toRadians(A));
-                      double  p = n * s;
-                               double Pi= p/ 2.0;
-        System.out.println(Pi);
-        System.out.println("I know this is wrong because Pi actually equals 3.14159 etc. But this is what I got.");
-        System.out.print("*finger guns*");
+        while (true) {
+            System.out.println("Please type in the number of sides");
+            Scanner sc = new Scanner(System.in);
 
+            int n = sc.nextInt();
+            if (n < 1) {
+                break;
+            }
+
+            double b = 360.0 / n;
+            double A = (1.0 / 2.0) * b;
+            double s = 2.0 * Math.sin(Math.toRadians(A));
+            double p = n * s;
+            double Pi = p / 2.0;
+            System.out.print("My Pi estimate is ");
+            System.out.println(Pi);
+            System.out.println("The higher the number suggested, the more accurate the estimate of Pi");
+
+        }
     }
 }
