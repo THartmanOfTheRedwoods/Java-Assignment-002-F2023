c42d69f17d6282ebc31c1595e5caaede557905de
GinaD14 <adanna534@mycr.redwoods.edu>
2023-09-30 18:00:18 -0700

    Gina Assignment 2

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter the number of sides for a polygon: ");
+        int numSides = Integer.parseInt(s.nextLine());
+        double B = 360.0 / numSides;
+        double A = B / 2;
+        double side = 2 * Math.sin(Math.toRadians(A));
+        double p = numSides * side;
+        double myPI = p / 2;
+        System.out.printf("My estimate for PI: %.10f, myPI, Math.PI");
     }
-}
